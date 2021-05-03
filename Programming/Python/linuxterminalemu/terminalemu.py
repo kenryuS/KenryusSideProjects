85d3a95e39237befe54b80c05f10d437d6713eba
kenryuS <kenryushibata@gmail.com>
2021-05-02 21:36:36 -0700

    updated terminalemu.py

--- Programming/Python/linuxterminalemu/terminalemu.py
+++ Programming/Python/linuxterminalemu/terminalemu.py
@@ -1,15 +1,15 @@
 #This is the linux terminal emulator written in python script using os module
 import os
-import sleep
 
 #setup
 os.system('clear')
 cmd = ""
 cut = 0
 directory = ""
+version = "Version: 0.0.5 alpha"
 print("Welcome to pash terminal, the bash terminal emulator written in python!")
 print("The commands are same to the bash. Type [pelp] for more information.")
-print("Version: 0.0.1 alpha")
+print(version)
 
 #main operation
 while cut == 0:
@@ -17,16 +17,16 @@ while cut == 0:
     directory = direct.readline()
     cmd = input("python@pash:" + directory + ">>>")
     if cmd == "version":
-        print("Version: 0.0.1 alpha")
+        print(version)
     elif cmd == "exit":
         print("exitting from pash ...")
-        sleep(1.5)
         break
     elif cmd == "pelp":
         print("This is the python program that excecute the bash commands.")
         print("Here are the some commands that only for this terminal emulator.")
         print("[version] - shows the version of the pash terminal")
         print("[exit] - exit the pash terminal")
+        print("[pelp] - shows the help of the pash terminal")
         print("")
         print("Update: 5/1/2021 by kenryuS")
     else:
