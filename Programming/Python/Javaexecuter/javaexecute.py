c4557af5183c1a8ade8e430857fbc2850b3f7c27
kenryuS <75469910+kenryuS@users.noreply.github.com>
2021-06-01 17:53:22 -0700

    .

---
+++ Programming/Python/Javaexecuter/javaexecute.py
@@ -0,0 +1,14 @@
+#Java executer written in Python
+import os
+x = 0
+while x == 0:
+    path = ""
+    command = ""
+    errorind = ""
+    path = input('path to the java file: ')
+    command = "javac " + path
+    print("compiling code...")
+    os.system(command)
+    print("Running the code...")
+    command = "java " + path
+    os.system(command)
