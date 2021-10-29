6dd8185d53e1e4f697fc6de49fdf3d0b2bac06e1
kenryuS <kenryushibata@gmail.com>
2021-10-29 17:49:00 -0400

    Update boot_sector_hello.asm

--- Programming/Other/OSfromScratch/Bootsector/boot_sector_hello.asm
+++ Programming/Other/OSfromScratch/Bootsector/boot_sector_hello.asm
@@ -1,16 +1,16 @@
 mov ah, 0x0e ; tty mode
 mov al, 'H'
 int 0x10
-mov al 'e'
+mov al, 'e'
 int 0x10
 mov al, 'l'
 int 0x10
-int 0x10 ; Print 'l' again
+int 0x10 ; 'l' is still on al, remember?
 mov al, 'o'
 int 0x10
 
-jmp $ ; jump to current address
+jmp $ ; jump to current address = infinite loop
 
 ; padding and magic number
 times 510 - ($-$$) db 0
-dw 0xaa55
+dw 0xaa55 
