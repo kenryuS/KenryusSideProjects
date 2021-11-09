d10b4396e0cd9244bc27673d8cf0236a236d7197
kenryuS <75469910+kenryuS@users.noreply.github.com>
2021-11-08 20:38:51 -0500

    Update testinfpi3.py

--- Programming/Python/PIGUI(Py edition)/testinfpi3.py
+++ Programming/Python/PIGUI(Py edition)/testinfpi3.py
@@ -3,7 +3,7 @@ n = int(input('n = '))
 output = 0
 while i <= n:
     output += ((-1)**(i-1)) * 4/((2*i)-1)
-    print(str((i/n)*100)[:5], end="\r")
+    print(str((i/n)*100)[:5] + "%", end="\r")
     i += 1
 
 print("\n" + str(output))
