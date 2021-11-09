c84592f8bb8fbe20f1e8c563be15a953bb8a9808
kenryuS <75469910+kenryuS@users.noreply.github.com>
2021-11-08 20:18:21 -0500

    Update testinfpi1.py

--- Programming/Python/PIGUI(Py edition)/testinfpi1.py
+++ Programming/Python/PIGUI(Py edition)/testinfpi1.py
@@ -7,7 +7,6 @@ while i <= n:
     a = (i**2)**-1
     out += a
     i += 1
-    proc = str(i) + "/" + str(n)
-    print(proc, end='\r')
-print(math.sqrt(out * 6))
+    print(str(i/n * 100)[:5] + "%", end="\r")
+print("\n" + str(math.sqrt(out * 6)), end="\n")
 # reference: n = 1x10^8 will calculate 3.1415926 x4498239000 in hour
